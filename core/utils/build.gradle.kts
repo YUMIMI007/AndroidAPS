7b960dd42dc8b909153c9de44e7b6f5e8f2b9f8e
YUMIMI007 <kilyoona100444@gmail.com>
2025-06-19 17:08:54 +0900

    Update build.gradle.kts

--- core/utils/build.gradle.kts
+++ core/utils/build.gradle.kts
@@ -11,6 +11,15 @@ plugins {
 android {
     namespace = "app.aaps.core.utils"
 }
+compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
 
 dependencies {
 
