b83f4a18b8f868e85ff3b05f481f09e9389b8b10
YUMIMI007 <kilyoona100444@gmail.com>
2025-06-19 16:57:13 +0900

    Update build.gradle.kts

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -12,11 +12,11 @@ android {
      compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+     }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
-kotlinOptions {
-    jvmTarget = "17"
-}
-
 dependencies {
     api(libs.androidx.core)
     api(libs.androidx.appcompat)
