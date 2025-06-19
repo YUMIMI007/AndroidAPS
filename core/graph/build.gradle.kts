eb96c53213457a8a38db8a688da875d97d2dc1b6
YUMIMI007 <kilyoona100444@gmail.com>
2025-06-19 17:14:33 +0900

    Update build.gradle.kts

--- core/graph/build.gradle.kts
+++ core/graph/build.gradle.kts
@@ -10,6 +10,15 @@ android {
 
     namespace = "app.aaps.core.graph"
 }
+compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
 
 dependencies {
     implementation(project(":core:data"))
